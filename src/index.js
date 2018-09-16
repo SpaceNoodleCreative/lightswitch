92ed0ee8d9ce88b6da1b7e24c0d0358c999fd41c
Tamara Yadvichuk <tomsyadvichuks@users.noreply.github.com>
2018-09-16 18:19:32 +0200

    Removed service worker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import registerServiceWorker from "./registerServiceWorker"
-
-ReactDOM.render(<App />, document.getElementById("root"))
-registerServiceWorker()
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+ReactDOM.render(<App />, document.getElementById("root"));
